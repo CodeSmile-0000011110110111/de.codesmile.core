40f0cfa792c0b23411df6b9eceb6df03f98ea6f7
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-05-09 12:45:34 +0200

    Log events: add parameters

--- Runtime/Components/Utility/LogUnityEvents.cs
+++ Runtime/Components/Utility/LogUnityEvents.cs
@@ -13,127 +13,82 @@ namespace CodeSmile.Components.Utility
 		[SerializeField] private Boolean m_LogRepeatingEvents;
 
 		private void Awake() => LogMethod("Awake");
-
 		private void Reset() => LogMethod("Reset");
-
 		private void Start() => LogMethod("Start");
-
 		private void Update()
 		{
 			if (m_LogRepeatingEvents)
 				LogMethod("Update");
 		}
-
 		private void FixedUpdate()
 		{
 			if (m_LogRepeatingEvents)
 				LogMethod("FixedUpdate");
 		}
-
 		private void LateUpdate()
 		{
 			if (m_LogRepeatingEvents)
 				LogMethod("LateUpdate");
 		}
-
 		private void OnEnable() => LogMethod("OnEnable");
-
 		private void OnDisable() => LogMethod("OnDisable");
-
 		private void OnDestroy() => LogMethod("OnDestroy");
-
 		private void OnGUI()
 		{
 			if (m_LogRepeatingEvents)
 				LogMethod($"OnGUI ({Event.current.type})");
 		}
-
 		private void OnAnimatorIK(Int32 layerIndex)
 		{
 			if (m_LogRepeatingEvents)
 				LogMethod("OnAnimatorIK");
 		}
-
 		private void OnAnimatorMove()
 		{
 			if (m_LogRepeatingEvents)
 				LogMethod("OnAnimatorMove");
 		}
-
-		private void OnApplicationFocus(Boolean hasFocus) => LogMethod("OnApplicationFocus");
-
-		private void OnApplicationPause(Boolean pauseStatus) => LogMethod("OnApplicationPause");
-
+		private void OnApplicationFocus(Boolean hasFocus) => LogMethod($"OnApplicationFocus: {hasFocus}");
+		private void OnApplicationPause(Boolean pauseStatus) => LogMethod("OnApplicationPause: {pauseStatus}");
 		private void OnApplicationQuit() => LogMethod("OnApplicationQuit");
-
-		private void OnAudioFilterRead(Single[] data, Int32 channels) => LogMethod("OnAudioFilterRead");
-
+		private void OnAudioFilterRead(Single[] data, Int32 channels) => LogMethod($"OnAudioFilterRead: {data} ({data?.Length}), channels: {channels})");
 		private void OnBecameInvisible() => LogMethod("OnBecameInvisible");
-
 		private void OnBecameVisible() => LogMethod("OnBecameVisible");
-
 		private void OnBeforeTransformParentChanged() => LogMethod("OnBeforeTransformParentChanged");
-
 		private void OnCanvasGroupChanged() => LogMethod("OnCanvasGroupChanged");
-
 		private void OnCanvasHierarchyChanged() => LogMethod("OnCanvasHierarchyChanged");
-
-		private void OnCollisionEnter(Collision other) => LogMethod("OnCollisionEnter");
-
-		private void OnCollisionEnter2D(Collision2D other) => LogMethod("OnCollisionEnter2D");
-
-		private void OnCollisionExit(Collision other) => LogMethod("OnCollisionExit");
-
-		private void OnCollisionExit2D(Collision2D other) => LogMethod("OnCollisionExit2D");
-
-		private void OnCollisionStay(Collision other) => LogMethod("OnCollisionStay");
-
-		private void OnCollisionStay2D(Collision2D other) => LogMethod("OnCollisionStay2D");
-
+		private void OnCollisionEnter(Collision other) => LogMethod($"OnCollisionEnter: {other}");
+		private void OnCollisionEnter2D(Collision2D other) => LogMethod($"OnCollisionEnter2D: {other}");
+		private void OnCollisionExit(Collision other) => LogMethod($"OnCollisionExit: {other}");
+		private void OnCollisionExit2D(Collision2D other) => LogMethod($"OnCollisionExit2D: {other}");
+		private void OnCollisionStay(Collision other) => LogMethod($"OnCollisionStay: {other}");
+		private void OnCollisionStay2D(Collision2D other) => LogMethod($"OnCollisionStay2D: {other}");
 		private void OnConnectedToServer() => LogMethod("OnConnectedToServer");
-
-		private void OnControllerColliderHit(ControllerColliderHit hit) => LogMethod("OnControllerColliderHit");
-
+		private void OnControllerColliderHit(ControllerColliderHit hit) => LogMethod($"OnControllerColliderHit: {hit}");
 		private void OnDidApplyAnimationProperties() => LogMethod("OnDidApplyAnimationProperties");
-
 		private void OnDrawGizmos()
 		{
 			if (m_LogRepeatingEvents)
 				LogMethod("OnDrawGizmos");
 		}
-
 		private void OnDrawGizmosSelected()
 		{
 			if (m_LogRepeatingEvents)
 				LogMethod("OnDrawGizmosSelected");
 		}
-
-		private void OnJointBreak(Single breakForce) => LogMethod("OnJointBreak");
-
-		private void OnJointBreak2D(Joint2D brokenJoint) => LogMethod("OnJointBreak2D");
-
+		private void OnJointBreak(Single breakForce) => LogMethod($"OnJointBreak: {breakForce}");
+		private void OnJointBreak2D(Joint2D brokenJoint) => LogMethod($"OnJointBreak2D: {brokenJoint}");
 		private void OnMouseDown() => LogMethod("OnMouseDown");
-
 		private void OnMouseDrag() => LogMethod("OnMouseDrag");
-
 		private void OnMouseEnter() => LogMethod("OnMouseEnter");
-
 		private void OnMouseExit() => LogMethod("OnMouseExit");
-
 		private void OnMouseOver() => LogMethod("OnMouseOver");
-
 		private void OnMouseUp() => LogMethod("OnMouseUp");
-
 		private void OnMouseUpAsButton() => LogMethod("OnMouseUpAsButton");
-
-		private void OnParticleCollision(GameObject other) => LogMethod("OnParticleCollision");
-
+		private void OnParticleCollision(GameObject other) => LogMethod($"OnParticleCollision: {other}");
 		private void OnParticleSystemStopped() => LogMethod("OnParticleSystemStopped");
-
 		private void OnParticleTrigger() => LogMethod("OnParticleTrigger");
-
 		private void OnParticleUpdateJobScheduled() => LogMethod("OnParticleUpdateJobScheduled");
-
 		private void OnPostRender()
 		{
 			if (m_LogRepeatingEvents)
@@ -153,31 +108,18 @@ namespace CodeSmile.Components.Utility
 		}
 
 		private void OnRectTransformDimensionsChange() => LogMethod("OnRectTransformDimensionsChange");
-
-		private void OnRenderImage(RenderTexture source, RenderTexture destination) => LogMethod("OnRenderImage");
-
+		private void OnRenderImage(RenderTexture source, RenderTexture destination) => LogMethod($"OnRenderImage: {source}, {destination}");
 		private void OnRenderObject() => LogMethod("OnRenderObject");
-
 		private void OnServerInitialized() => LogMethod("OnServerInitialized");
-
 		private void OnTransformChildrenChanged() => LogMethod("OnTransformChildrenChanged");
-
 		private void OnTransformParentChanged() => LogMethod("OnTransformParentChanged");
-
-		private void OnTriggerEnter(Collider other) => LogMethod("OnTriggerEnter");
-
-		private void OnTriggerEnter2D(Collider2D other) => LogMethod("OnTriggerEnter2D");
-
-		private void OnTriggerExit(Collider other) => LogMethod("OnTriggerExit");
-
-		private void OnTriggerExit2D(Collider2D other) => LogMethod("OnTriggerExit2D");
-
-		private void OnTriggerStay(Collider other) => LogMethod("OnTriggerStay");
-
-		private void OnTriggerStay2D(Collider2D other) => LogMethod("OnTriggerStay2D");
-
+		private void OnTriggerEnter(Collider other) => LogMethod($"OnTriggerEnter: {other}");
+		private void OnTriggerEnter2D(Collider2D other) => LogMethod($"OnTriggerEnter2D: {other}");
+		private void OnTriggerExit(Collider other) => LogMethod($"OnTriggerExit: {other}");
+		private void OnTriggerExit2D(Collider2D other) => LogMethod($"OnTriggerExit2D: {other}");
+		private void OnTriggerStay(Collider other) => LogMethod($"OnTriggerStay: {other}");
+		private void OnTriggerStay2D(Collider2D other) => LogMethod($"OnTriggerStay2D: {other}");
 		private void OnValidate() => LogMethod("OnValidate");
-
 		private void OnWillRenderObject()
 		{
 			if (m_LogRepeatingEvents)
