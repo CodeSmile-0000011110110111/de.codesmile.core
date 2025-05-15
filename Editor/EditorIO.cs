311d8a1900ba452c2a510859dd037e8725a9600a
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-05-15 12:36:42 +0200

    move MonoSingleton, add EditorIO

---
+++ Editor/EditorIO.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (C) 2021-2025 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using CodeSmile.Extensions.System;
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmileEditor.Core
+{
+	public static class EditorIO
+	{
+		public static void TryCreateDirectory(String assetPath)
+		{
+			var generatedFullPath = Path.GetFullPath($"{Application.dataPath}/../{assetPath}");
+			if (!Directory.Exists(generatedFullPath))
+			{
+				Directory.CreateDirectory(generatedFullPath);
+				AssetDatabase.ImportAsset(assetPath);
+			}
+		}
+
+		public static String GetFullPathFromAssetPath(String assetPath) =>
+			Path.GetFullPath($"{Application.dataPath}/../{assetPath}").ToForwardSlashes();
+	}
+}
