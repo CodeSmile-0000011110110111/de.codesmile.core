b909af4e7bb7e49c5baa4c7609c3f9bd17a5ff5c
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-06 19:42:55 +0200

    add AssetUtility, FileUtility, PathUtility

---
+++ Editor/AssetUtility.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (C) 2021-2025 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using CodeSmile.Extensions.System;
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace CodeSmileEditor.Core
+{
+	public static class AssetUtility
+	{
+		private static Assembly[] s_Assemblies;
+		private static readonly Int32 s_DllStringLength = ".dll".Length;
+
+		public static Boolean IsEditorPath(String assetPath) => assetPath.ToLower().Contains("/editor/");
+
+		public static Boolean IsEditorAssembly(String assetPath)
+		{
+			var assemblyName = CompilationPipeline.GetAssemblyNameFromScriptPath(assetPath);
+			if (assemblyName != null && assemblyName.Length > s_DllStringLength)
+			{
+				// strip ".dll" from name
+				assemblyName = assemblyName.Substring(0, assemblyName.Length - s_DllStringLength);
+
+				foreach (var assembly in s_Assemblies)
+				{
+					if ((assembly.flags & AssemblyFlags.EditorAssembly) != 0 && assemblyName.Equals(assembly.name))
+						return true;
+				}
+			}
+
+			return false;
+		}
+
+		[InitializeOnLoadMethod]
+		private static void OnLoad() => s_Assemblies = CompilationPipeline.GetAssemblies();
+	}
+}
---
+++ Runtime/Utility/PathUtility.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (C) 2021-2025 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using CodeSmile.Extensions.System;
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmile.Utility
+{
+	public static class PathUtility
+	{
+		public static String GetRelativePath(String path) =>
+			Path.GetRelativePath(Application.dataPath, path).ToForwardSlashes();
+
+		public static String GetFullPath(String rootPath, String relativePath) =>
+			Path.GetFullPath(Path.Join(rootPath, relativePath)).ToForwardSlashes();
+
+		public static void TryCreateDirectory(String path)
+		{
+			try
+			{
+				path = Path.GetFullPath(path).ToForwardSlashes();
+				if (Directory.Exists(path) == false)
+					Directory.CreateDirectory(path);
+			}
+			catch (Exception e)
+			{
+				throw new IOException($"Failed to create directory '{path}', reason: {e}", e);
+			}
+		}
+	}
+}
