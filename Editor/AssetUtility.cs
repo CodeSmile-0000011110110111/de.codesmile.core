5899883347779aa0a360e0a27e8b68168d64ead8
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-13 12:15:48 +0200

    AssetUtility.Import and RuntimeAssetUtility to safely call it from runtime code

--- Editor/AssetUtility.cs
+++ Editor/AssetUtility.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (C) 2021-2025 Steffen Itterheim
 // Refer to included LICENSE file for terms and conditions.
 
-using CodeSmile.Extensions.System;
 using System;
 using System.IO;
 using UnityEditor;
 using UnityEditor.Compilation;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace CodeSmileEditor.Core
 {
@@ -15,9 +15,32 @@ namespace CodeSmileEditor.Core
 		private static Assembly[] s_Assemblies;
 		private static readonly Int32 s_DllStringLength = ".dll".Length;
 
+		/// <summary>
+		/// Test if the path points to a Lua script (.lua extension).
+		/// </summary>
+		/// <param name="assetPath"></param>
+		/// <returns></returns>
 		public static Boolean IsLuaScript(String assetPath) => Path.GetExtension(assetPath) == ".lua";
+
+		/// <summary>
+		/// Test if the path is in an "Editor" folder, or a child thereof.
+		/// </summary>
+		/// <param name="assetPath"></param>
+		/// <returns></returns>
 		public static Boolean IsEditorPath(String assetPath) => assetPath.ToLower().Contains("/editor/");
+
+		/// <summary>
+		/// Test if the path is in a "Modding" folder, or a child thereof.
+		/// </summary>
+		/// <param name="assetPath"></param>
+		/// <returns></returns>
 		public static Boolean IsModdingPath(String assetPath) => assetPath.ToLower().Contains("/modding/");
+
+		/// <summary>
+		/// Test if the current path is within an editor assembly definition. Does not check for "Editor" folders.
+		/// </summary>
+		/// <param name="assetPath"></param>
+		/// <returns></returns>
 		public static Boolean IsEditorAssembly(String assetPath)
 		{
 			var assemblyName = CompilationPipeline.GetAssemblyNameFromScriptPath(assetPath);
@@ -34,6 +57,17 @@ namespace CodeSmileEditor.Core
 			}
 
 			return false;
+		}
+
+		/// <summary>
+		/// Re-Imports the asset. Useful if auto-refresh is disabled and you want to pick up latest changes
+		/// without forcing an entire Refresh().
+		/// </summary>
+		/// <param name="asset"></param>
+		public static void Import(Object asset)
+		{
+			if (asset != null)
+				AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(asset));
 		}
 
 		[InitializeOnLoadMethod]
---
+++ Runtime/Utility/RuntimeAssetUtility.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (C) 2021-2025 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using UnityEditor;
+using UnityEngine;
+
+namespace CodeSmile.Utility
+{
+	public class RuntimeAssetUtility
+	{
+		/// <summary>
+		/// Editor-only: Imports the asset, ie to pick up latest changes without forcing an entire Refresh().
+		/// </summary>
+		/// <param name="asset"></param>
+		public static void Import(Object asset)
+		{
+#if UNITY_EDITOR
+			if (asset != null)
+				AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(asset));
+#endif
+		}
+	}
+}
